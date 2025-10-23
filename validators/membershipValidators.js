c471fc51393431daa2023fe54cccdc50dfc26a6a
jdgmn <20140976@s.ubaguio.edu>
2025-10-24 07:08:02 +0800

    emergency patch for expess validator's vulnerability

--- validators/membershipValidators.js
+++ validators/membershipValidators.js
@@ -1,6 +1,15 @@
 const { body } = require("express-validator");
+const validateUrl = require("./urlValidator");
 
 exports.createMembership = [
+  // optional website field validated with safe helper instead of express-validator's isURL
+  body("website")
+    .optional()
+    .custom((v) => {
+      if (!validateUrl(v)) throw new Error("Invalid website URL");
+      return true;
+    }),
+
   body("email").isEmail().withMessage("Valid user email required").normalizeEmail(),
   body("membershipType")
     .isIn(["monthly", "quarterly", "annual"])
