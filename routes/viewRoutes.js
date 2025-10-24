6b8e22e447d0e6c3fc30d32e7804287ebd0a7e5a
jdgmn <20140976@s.ubaguio.edu>
2025-10-24 08:20:00 +0800

    added missing functionality

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,54 +3,38 @@ const router = express.Router();
 const User = require("../models/User");
 const Membership = require("../models/Membership");
 const Checkin = require("../models/Checkin");
-const { protect, restrictTo } = require("../middlewares/authMiddleware");
 
 // Public pages
 router.get("/", (req, res) => res.render("login"));
 router.get("/register", (req, res) => res.render("register"));
 
-// Dashboard view (no server-side protect)
+// Dashboard view (public for dev)
 router.get("/dashboard", async (req, res) => {
   res.render("dashboard");
 });
 
-// Users page
-router.get(
-  "/users",
-  protect,
-  restrictTo("admin", "receptionist"),
-  async (req, res) => {
-    const users = await User.find().select("firstName lastName email role");
-    res.render("users", { users });
-  }
-);
+// Users page (public for dev)
+router.get("/users", async (req, res) => {
+  const users = await User.find().select("firstName lastName email role");
+  res.render("users", { users });
+});
 
-// Memberships
-router.get(
-  "/memberships",
-  protect,
-  restrictTo("admin", "receptionist"),
-  async (req, res) => {
-    const memberships = await Membership.find().populate(
-      "user",
-      "firstName lastName email"
-    );
-    res.render("memberships", { memberships });
-  }
-);
+// Memberships (public for dev)
+router.get("/memberships", async (req, res) => {
+  const memberships = await Membership.find().populate(
+    "user",
+    "firstName lastName email"
+  );
+  res.render("memberships", { memberships });
+});
 
-// Check-ins
-router.get(
-  "/checkins",
-  protect,
-  restrictTo("admin", "receptionist"),
-  async (req, res) => {
-    const checkins = await Checkin.find().populate(
-      "user",
-      "firstName lastName email"
-    );
-    res.render("checkins", { checkins });
-  }
-);
+// Check-ins (public for dev)
+router.get("/checkins", async (req, res) => {
+  const checkins = await Checkin.find().populate(
+    "user",
+    "firstName lastName email"
+  );
+  res.render("checkins", { checkins });
+});
 
 module.exports = router;
